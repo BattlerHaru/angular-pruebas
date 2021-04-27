6b73b2836dbb2e7ad1287016bdbbe22147f9c2c5
V-Haru <kronk.sdance@gmail.com>
2021-04-26 21:27:43 -0500

    Pruebas intermedias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// Pruebas de espias
+import { MedicosComponent } from './intermedio/espias/medicos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MedicosComponent
   ],
   imports: [
     BrowserModule
--- src/app/basicas/arreglos/arreglos.spec.ts
+++ src/app/basicas/arreglos/arreglos.spec.ts
@@ -1,6 +1,7 @@
 import { obtenerRobots } from './arreglos';
 
-describe('Pruebas de arreglos', () => {
+// Para omitir una prueba se agrega una x antes de la prueba, ya sea antes del describe o antes del it
+xdescribe('Pruebas de arreglos', () => {
 
     it('Debe de retornar al menos 3 robots', () => {
         const resp = obtenerRobots();
@@ -9,8 +10,8 @@ describe('Pruebas de arreglos', () => {
         // En caso de que tenga mas de 3 debera ser toBeGreaterThanOrEqual(3) que significa que debera tener mas de 3 o igual a 3
         expect(resp.length).toBeGreaterThanOrEqual(3);
     });
-
-    it('Bebe de existir: MegaMan y Ultron', () => {
+    // xit()
+    it('Debe de existir: MegaMan y Ultron', () => {
         const resp = obtenerRobots();
 
         expect(resp).toContain('MegaMan');
